c597791e7d85e4f3fd85993e4d90f63a095831cc
xu yaxiong <xyxlindy@163.com>
2023-08-21 17:15:50 +0800

    添加顺序查找

---
+++ src/main/java/org/example/chapter_3/SequentialSearchST.java
@@ -0,0 +1,43 @@
+package org.example.chapter_3;
+
+public class SequentialSearchST<Key, Value> {
+    private class Node {
+        Key key;
+        Value val;
+        Node next;
+
+        public Node(Key key, Value val, Node next) {
+            this.key = key;
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    private Node first;
+
+    public Value get(Key key) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key))
+                return x.val;
+        }
+        return null;
+    }
+
+    public void put(Key key, Value val) {
+        for (Node x = first; x != null; x = x.next) {
+            if (key.equals(x.key)) {
+                x.val = val;
+                return;
+            }
+        }
+        first = new Node(key, val, first);
+    }
+
+    public static void main(String[] args) {
+        SequentialSearchST<String, Integer> st = new SequentialSearchST<>();
+        st.put("hello", 1);
+        st.put("world", 2);
+        st.put("hello", 3);
+        System.out.println(st);
+    }
+}
