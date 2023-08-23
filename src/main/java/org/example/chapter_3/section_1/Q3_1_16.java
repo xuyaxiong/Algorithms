685406e66265697f40ed8d07564ae0e1b8d3ce24
xu yaxiong <xyxlindy@163.com>
2023-08-23 11:21:23 +0800

    提交练习3.1.16

--- src/main/java/org/example/chapter_3/section_1/Q3_1_16.java
+++ src/main/java/org/example/chapter_3/section_1/Q3_1_16.java
@@ -1,14 +1,15 @@
 package org.example.chapter_3.section_1;
 
+import edu.princeton.cs.algs4.StdOut;
 import org.example.chapter_1.section_3.Queue;
 
-public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Key, Value> {
+public class Q3_1_16<Key extends Comparable<Key>, Value> implements ST<Key, Value> {
     private static final int INIT_CAPACITY = 2;
     private Key[] keys;
     private Value[] vals;
     private int N;
 
-    public BinarySearchST() {
+    public Q3_1_16() {
         keys = (Key[]) new Comparable[INIT_CAPACITY];
         vals = (Value[]) new Object[INIT_CAPACITY];
     }
@@ -96,5 +97,32 @@ public class BinarySearchST<Key extends Comparable<Key>, Value> implements ST<Ke
         for (int i = 0; i < N; ++i)
             q.enqueue(keys[i]);
         return q;
+    }
+
+    public static void main(String[] args) {
+        Q3_1_16<String, Double> q3116 = new Q3_1_16<>();
+        q3116.put("A+", 4.33);
+        q3116.put("A", 4.00);
+        q3116.put("A-", 3.67);
+        q3116.put("B+", 3.33);
+        q3116.put("B", 3.00);
+        q3116.put("B-", 2.67);
+        q3116.put("C+", 2.33);
+        q3116.put("C", 2.00);
+        q3116.put("C-", 1.67);
+        q3116.put("D", 1.00);
+        q3116.put("F", 0.00);
+
+        q3116.delete("A+");
+        q3116.delete("A-");
+        q3116.delete("B+");
+        q3116.delete("B-");
+        q3116.delete("C+");
+        q3116.delete("C-");
+        q3116.delete("F");
+
+        for (String key : q3116.keys()) {
+            StdOut.println(key + " " + q3116.get(key));
+        }
     }
 }
