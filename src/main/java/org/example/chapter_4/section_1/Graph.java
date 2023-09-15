2b8dcec394da118af17b9490b6201060efa66420
xuyaxiong <xyxlindy@163.com>
2023-09-15 11:22:00 +0800

    添加Graph

---
+++ src/main/java/org/example/chapter_4/section_1/Graph.java
@@ -0,0 +1,89 @@
+package org.example.chapter_4.section_1;
+
+import edu.princeton.cs.algs4.In;
+import org.example.chapter_1.section_3.Bag;
+
+public class Graph {
+
+    private final int V;
+    private int E;
+    private Bag<Integer>[] adj;
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<>();
+    }
+
+    public Graph(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public static int degree(Graph G, int v) {
+        int degree = 0;
+        for (int w : G.adj(v)) degree++;
+        return degree;
+    }
+
+    public static int maxDegree(Graph G) {
+        int max = 0;
+        for (int v = 0; v < G.V(); v++)
+            if (degree(G, v) > max)
+                max = degree(G, v);
+        return max;
+    }
+
+    public static double avgDegree(Graph G) {
+        return 2.0 * G.E() / G.V();
+    }
+
+    public static int numberOfSelfLoops(Graph G) {
+        int count = 0;
+        for (int v = 0; v < G.V(); v++)
+            for (int w : G.adj(v))
+                if (v == w) count++;
+        return count / 2;
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+    public String toString() {
+        String s = V + " vertices, " + E + " edges\n";
+        for (int v = 0; v < V; v++) {
+            s += v + ": ";
+            for (int w : adj(v))
+                s += w + " ";
+            s += "\n";
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        Graph G = new Graph(new In(args[0]));
+        System.out.println(G);
+    }
+}
